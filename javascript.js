e57831a1e497e0a156444ee58e1592fb64b8b5dc
DanielRusinov <daniel.rusinov00@gmail.com>
2018-05-14 23:06:09 +0300

    I

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var element = document.getElementById("video");
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
